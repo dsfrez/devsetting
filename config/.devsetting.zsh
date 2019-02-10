4fb2e538a849415d70f1dfa1dfff493513781a55
Yoon Jinyong <dsfrez@gmail.com>
2019-02-10 21:38:59 +0900

    INCOMPLETE - zsh theme

--- config/.devsetting.zsh
+++ config/.devsetting.zsh
@@ -26,6 +26,18 @@ antigen bundle uvaes/fzf-marks
 
 antigen apply
 
+eval `dircolors $DEVSETTING_DIR/git/dircolors-solarized/dircolors.256dark`
+source $DEVSETTING_DIR/git/mintty-colors-solarized/sol.dark
+
+# show available tmux sessions - https://jamesoff.net/2017/08/26/tmux-configuration.html
+if [[ -z $TMUX ]]; then
+    sessions=$( tmux ls 2> /dev/null | awk '! /attached/ { sub(":", "", $1); print $1; }' | xargs echo )
+    if [[ ! -z $sessions ]]; then
+        echo "==> Available tmux sessions: $sessions"
+    fi
+    unset sessions
+fi
+
 source $DEVSETTING_DIR/config/.devsetting.common
 
 # Aliases ---------------------------------------------------------------------
